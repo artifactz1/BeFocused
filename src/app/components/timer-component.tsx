b3f21b25084b3510261f815566421f7afedecaf4
artifactz1 <punsalang.art@gmail.com>
2023-08-02 22:10:56 -0700

    Timer-Component: was able to achieve visualization - need to fix how it looks. It is kind of too stretched at the moment

--- src/app/components/timer-component.tsx
+++ src/app/components/timer-component.tsx
@@ -36,15 +36,18 @@ export default function TimerComponent() {
   const displaySeconds = remainingTime % 60;
 
   return (
-    <div className="bg-gray-200 w-1/2 h-screen flex items-end justify-center rounded-tr-[30px] rounded-br-[30px]">
+    <div className="bg-gray-200 w-1/2 h-screen flex items-end justify-center rounded-[30px] overflow-hidden relative">
       <div
         className="bg-red-500 h-full w-full transition-transform duration-1000 origin-bottom"
-        style={{ transform: `scaleY(${progress / 100})` }}
-      >
-        <div className="text-black mt-2 ">
-          {displayMinutes.toString().padStart(2, "0")}:
-          {displaySeconds.toString().padStart(2, "0")}
-        </div>
+        style={{
+          transform: `scaleY(${progress / 100})`,
+          transformOrigin: "bottom",
+        }}
+      ></div>
+
+      <div className="text-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        {displayMinutes.toString().padStart(2, "0")}:
+        {displaySeconds.toString().padStart(2, "0")}
       </div>
     </div>
   );

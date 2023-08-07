316e2fd5b70cb2ed746b99d3ff9aad1f5a74265a
artifactz1 <punsalang.art@gmail.com>
2023-08-07 00:53:02 -0700

    Timer/Wave component : fixed the error of user zooming in and out, it will dynamically always be the same size - timer being 90% and wave being 10% of screen

--- src/app/components/timer-component.tsx
+++ src/app/components/timer-component.tsx
@@ -1,239 +1,11 @@
-// "use client";
-
-// import { useState, useEffect } from "react";
-// import { AiFillPauseCircle } from "react-icons/ai";
-
-// export default function TimerComponent() {
-//   const minutes = 0;
-//   const seconds = 10;
-
-//   // Convert the total duration to seconds
-//   const totalDuration = minutes * 60 + seconds;
-
-//   const [progress, setProgress] = useState(100);
-//   const [remainingTime, setRemainingTime] = useState(totalDuration);
-
-//   useEffect(() => {
-//     let currentTime = totalDuration;
-
-//     const timerInterval = setInterval(() => {
-//       currentTime -= 1;
-//       const newProgress = (currentTime / totalDuration) * 100;
-//       setProgress(newProgress);
-//       setRemainingTime(currentTime);
-
-//       if (currentTime <= 0) {
-//         console.log("Timer has completed!");
-//         clearInterval(timerInterval);
-//       }
-//     }, 1000);
-
-//     return () => clearInterval(timerInterval);
-//   }, [totalDuration]);
-
-//   // Format the remaining time in minutes and seconds
-//   const displayMinutes = Math.floor(remainingTime / 60);
-//   const displaySeconds = remainingTime % 60;
-
-//   return (
-//     <div className="w-1/2 h-screen p-4">
-//       <div className="bg-gray-200 w-full h-full flex items-end justify-center rounded-[30px] overflow-hidden relative border-[2px] border-black">
-//         <div
-//           className="bg-red-500 h-full w-full transition-transform duration-1000 origin-bottom"
-//           style={{
-//             transform: `scaleY(${progress / 100})`,
-//             transformOrigin: "bottom",
-//           }}
-//         ></div>
-
-//         <div className="text-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-//           {displayMinutes.toString().padStart(2, "0")}:
-//           {displaySeconds.toString().padStart(2, "0")}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// "use-client";
-
-// // Milliseconds Added
-
-// import React, { useState, useEffect } from "react";
-// import { AiFillPauseCircle } from "react-icons/ai";
-
-// export default function TimerComponent() {
-//   const minutes = 0;
-//   const seconds = 10;
-
-//   // Convert the total duration to seconds
-//   const totalDuration = minutes * 60 + seconds;
-
-//   const [progress, setProgress] = useState(100);
-//   const [remainingTime, setRemainingTime] = useState(totalDuration * 1000); // Convert to milliseconds
-
-//   useEffect(() => {
-//     let currentTime = totalDuration * 1000; // Convert to milliseconds
-
-//     const timerInterval = setInterval(() => {
-//       currentTime -= 10; // Decrease by 10 milliseconds
-//       const newProgress = (currentTime / (totalDuration * 1000)) * 100; // Convert back to seconds and calculate progress
-//       setProgress(newProgress);
-//       setRemainingTime(currentTime);
-
-//       if (currentTime <= 0) {
-//         console.log("Timer has completed!");
-//         clearInterval(timerInterval);
-//       }
-//     }, 10); // Update every 10 milliseconds
-
-//     return () => clearInterval(timerInterval);
-//   }, [totalDuration]);
-
-//   // Format the remaining time in minutes, seconds, and milliseconds
-//   const displayMinutes = Math.floor(remainingTime / (60 * 1000));
-//   const displaySeconds = Math.floor((remainingTime % (60 * 1000)) / 1000);
-//   //   const displayMilliseconds = remainingTime % 1000;
-
-//   return (
-//     <div className="w-1/2 h-screen p-4">
-//       <div className="bg-gray-200 w-full h-full flex items-end justify-center rounded-[30px] overflow-hidden relative border-[2px] border-black">
-//         <div
-//           className="bg-red-500 h-full w-full transition-transform duration-10 origin-bottom"
-//           style={{
-//             transform: `scaleY(${remainingTime / (totalDuration * 1000)})`,
-//             transformOrigin: "bottom",
-//           }}
-//         ></div>
-
-//         <div className="text-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-//           {displayMinutes.toString().padStart(2, "0")}:
-//           {displaySeconds.toString().padStart(2, "0")}
-//           {/* {displayMilliseconds.toString().padStart(3, "0")} */}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// "use client";
-
-// import { useState, useEffect } from "react";
-// import { AiFillPauseCircle } from "react-icons/ai";
-// import RainEffect from "./rain-component";
-
-// export default function TimerComponent() {
-//   const minutes = 0;
-//   const seconds = 10;
-
-//   // Convert the total duration to seconds
-//   const totalDuration = minutes * 60 + seconds;
-
-//   const [progress, setProgress] = useState(100);
-//   const [remainingTime, setRemainingTime] = useState(totalDuration);
-
-//   useEffect(() => {
-//     let currentTime = totalDuration;
-
-//     const timerInterval = setInterval(() => {
-//       currentTime -= 1;
-//       const newProgress = (currentTime / totalDuration) * 100;
-//       setProgress(newProgress);
-//       setRemainingTime(currentTime);
-
-//       if (currentTime <= 0) {
-//         console.log("Timer has completed!");
-//         clearInterval(timerInterval);
-//       }
-//     }, 1000);
-
-//     return () => clearInterval(timerInterval);
-//   }, [totalDuration]);
-
-//   // Format the remaining time in minutes and seconds
-//   const displayMinutes = Math.floor(remainingTime / 60);
-//   const displaySeconds = remainingTime % 60;
-
-//   return (
-//     <div className="w-screen h-screen">
-//       <div
-//         className="bg-red-500 h-full w-full transition-transform duration-1000 origin-bottom"
-//         style={{
-//           transform: `scaleY(${progress / 100})`,
-//           transformOrigin: "top",
-//         }}
-//       >
-//       <div className="text-black text-[120px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-//         {displayMinutes.toString().padStart(2, "0")}:
-//         {displaySeconds.toString().padStart(2, "0")}
-//       </div>
-//     </div>
-//   );
-// }
-
-// import { useState, useEffect } from "react";
-// import { AiFillPauseCircle } from "react-icons/ai";
-// import RainEffect from "./rain-component";
-
-// export default function TimerComponent() {
-//   const minutes = 0;
-//   const seconds = 10;
-
-//   // Convert the total duration to seconds
-//   const totalDuration = minutes * 60 + seconds;
-
-//   const [progress, setProgress] = useState(100);
-//   const [remainingTime, setRemainingTime] = useState(totalDuration);
-
-//   useEffect(() => {
-//     let currentTime = totalDuration;
-
-//     const timerInterval = setInterval(() => {
-//       currentTime -= 1;
-//       const newProgress = (currentTime / totalDuration) * 100;
-//       setProgress(newProgress);
-//       setRemainingTime(currentTime);
-
-//       if (currentTime <= 0) {
-//         console.log("Timer has completed!");
-//         clearInterval(timerInterval);
-//       }
-//     }, 1000);
-
-//     return () => clearInterval(timerInterval);
-//   }, [totalDuration]);
-
-//   // Format the remaining time in minutes and seconds
-//   const displayMinutes = Math.floor(remainingTime / 60);
-//   const displaySeconds = remainingTime % 60;
-
-//   return (
-//     <div className="w-screen h-screen">
-//       <div
-//         className="bg-red-500 h-full w-full transition-transform duration-1000 origin-bottom"
-//         style={{
-//           transform: `scaleY(${progress / 100})`,
-//           transformOrigin: "bottom", // Changed transformOrigin from "top" to "bottom"
-//         }}
-//       ></div>
-//       <div className="text-black text-[120px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-//         {displayMinutes.toString().padStart(2, "0")}:
-//         {displaySeconds.toString().padStart(2, "0")}
-//       </div>
-//     </div>
-//   );
-// }
-
-"use client";
-
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { AiFillPauseCircle } from "react-icons/ai";
 import RainEffect from "./rain-component";
 import WaveComponent from "./wave-component";
 
-export default function TimerComponent() {
+const TimerComponent: React.FC = () => {
   const minutes = 0;
-  const seconds = 30;
+  const seconds = 10;
 
   // Convert the total duration to seconds
   const totalDuration = minutes * 60 + seconds;
@@ -257,7 +29,16 @@ export default function TimerComponent() {
       }
     }, 1000);
 
-    return () => clearInterval(timerInterval);
+    const handleResize = () => {
+      setScreenHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      clearInterval(timerInterval);
+      window.removeEventListener("resize", handleResize);
+    };
   }, [totalDuration]);
 
   // Format the remaining time in minutes and seconds
@@ -267,32 +48,149 @@ export default function TimerComponent() {
   // Calculate the position of the wave component
   const wavePosition = (progress / 100) * screenHeight;
 
-  // Calculate the position of the wave component
   return (
     <div className="w-screen h-screen">
       <div
-        className="bg-white h-full w-full transition-transform duration-1000 origin-bottom absolute"
+        className="bg-red-500 h-full w-full transition-transform  duration-1000 origin-top bottom-0 absolute"
         style={{
-          //   transform: `scaleY(${1 - progress / 100} `,
-          transform: `scaleY(${1 - progress / 100 - 0.06} `,
+          transform: `scaleY(${1 - progress / 100 - 0.1})`, // Adjust the scale to your preference
           transformOrigin: "bottom",
         }}
       />
 
       <div
-        className="waves absolute w-full transition-transform duration-1000 origin-bottom bottom-3"
+        className="waves absolute w-full h-[10vh] transition-transform duration-1000 origin-bottom "
         style={{
           transform: `translateY(${wavePosition}px)`, // Use translateY to position the wave component
-
           transformOrigin: "bottom",
         }}
       >
         <WaveComponent />
       </div>
+
       <div className="text-black text-[120px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         {displayMinutes.toString().padStart(2, "0")}:
         {displaySeconds.toString().padStart(2, "0")}
       </div>
     </div>
   );
-}
+};
+
+export default TimerComponent;
+
+// import React, { useState, useEffect } from "react";
+// import { AiFillPauseCircle } from "react-icons/ai";
+// import RainEffect from "./rain-component";
+// import WaveComponent from "./wave-component";
+
+// const TimerComponent: React.FC = () => {
+//   const [isPaused, setIsPaused] = useState(true);
+//   const [minutes, setMinutes] = useState(0);
+//   const [seconds, setSeconds] = useState(10);
+//   const totalDuration = minutes * 60 + seconds;
+//   const [progress, setProgress] = useState(100);
+//   const [remainingTime, setRemainingTime] = useState(totalDuration);
+//   const [screenHeight, setScreenHeight] = useState(window.innerHeight);
+
+//   useEffect(() => {
+//     let currentTime = totalDuration;
+
+//     const timerInterval = setInterval(() => {
+//       if (!isPaused) {
+//         currentTime -= 1;
+//         const newProgress = (currentTime / totalDuration) * 100;
+//         setProgress(newProgress);
+//         setRemainingTime(currentTime);
+
+//         if (currentTime <= 0) {
+//           console.log("Timer has completed!");
+//           clearInterval(timerInterval);
+//           setIsPaused(true); // Pause the timer when it completes
+//         }
+//       }
+//     }, 1000);
+
+//     const handleResize = () => {
+//       setScreenHeight(window.innerHeight);
+//     };
+
+//     window.addEventListener("resize", handleResize);
+
+//     return () => {
+//       clearInterval(timerInterval);
+//       window.removeEventListener("resize", handleResize);
+//     };
+//   }, [totalDuration, isPaused]);
+
+//   const handleStartPauseClick = () => {
+//     setIsPaused((prevState) => !prevState); // Toggle pause state
+//   };
+
+//   const handleMinutesChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setMinutes(Number(event.target.value));
+//   };
+
+//   const handleSecondsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setSeconds(Number(event.target.value));
+//   };
+
+//   const wavePosition = (progress / 100) * screenHeight;
+
+//   const displayMinutes = Math.floor(remainingTime / 60);
+//   const displaySeconds = remainingTime % 60;
+
+//   return (
+//     <div className="w-screen h-screen">
+//       <div
+//         className="bg-red-500 h-full w-full transition-transform duration-1000 origin-bottom absolute"
+//         style={{
+//           //   transform: `scaleY(${1 - progress / 100} `,
+//           transform: `scaleY(${1 - progress / 100 - 0.06} `,
+//           transformOrigin: "bottom",
+//         }}
+//       />
+//       <div
+//         className="waves absolute w-full transition-transform duration-1000 origin-bottom mb-[100px]"
+//         style={{
+//           bottom: 0,
+//           transform: `translateY(${wavePosition}px)`,
+//         }}
+//       >
+//         <WaveComponent />
+//       </div>
+
+//       <div className="text-black text-[120px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+//         {displayMinutes.toString().padStart(2, "0")}:
+//         {displaySeconds.toString().padStart(2, "0")}
+//       </div>
+
+//       {/* Input fields for minutes and seconds */}
+//       <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2">
+//         <input
+//           type="number"
+//           value={minutes}
+//           onChange={handleMinutesChange}
+//           className="mr-2 px-2 py-1 border"
+//         />
+//         <input
+//           type="number"
+//           value={seconds}
+//           onChange={handleSecondsChange}
+//           className="px-2 py-1 border"
+//         />
+//       </div>
+
+//       {/* Start/Pause button */}
+//       <div className="absolute bottom-1/2 left-1/2 transform -translate-x-1/2">
+//         <button
+//           onClick={handleStartPauseClick}
+//           className="px-4 py-2 bg-blue-500 text-white"
+//         >
+//           {isPaused ? "Start" : "Pause"}
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default TimerComponent;
--- src/app/components/wave-component.tsx
+++ src/app/components/wave-component.tsx
@@ -3,13 +3,14 @@ import "../styles/waves.css"; // Import the CSS file
 
 const WaveComponent = () => {
   return (
-    <div className="waves absolute top-0 left-0 w-full">
+    <div className="waves absolute top-0 left-0 w-full h-full">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox="0 24 150 28"
+        // viewBox="0 24 150 28"
+        viewBox="0 25 150 28"
         preserveAspectRatio="none"
-        className="w-full h-20"
+        className="w-full h-[10vh]"
       >
         <defs>
           <path
@@ -36,7 +37,18 @@ const WaveComponent = () => {
             y="5"
             fill="rgba(255,255,255,0.3)"
           />
-          <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
+          <use
+            xlinkHref="#gentle-wave"
+            x="48"
+            y="6.5"
+            fill="rgba(255,255,255,0.5)"
+          />
+          <use
+            xlinkHref="#gentle-wave"
+            x="48"
+            y="9"
+            fill="rgba(255,255,255,0.5)"
+          />
         </g>
       </svg>
     </div>

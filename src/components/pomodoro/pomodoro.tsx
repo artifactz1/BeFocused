5d41abc60462a197f0d96ce36b0adf74d4d3801b
zachuri <punsalang.zachary@gmail.com>
2023-10-13 10:02:03 -0700

    fade-rain: added framer-motion -> able to display rain to display in and out

--- src/components/pomodoro/pomodoro.tsx
+++ src/components/pomodoro/pomodoro.tsx
@@ -4,12 +4,25 @@ import React, { useState } from 'react';
 import Rain from './rain';
 import Timer from './timer';
 
+import { motion, AnimatePresence } from 'framer-motion';
+
 const Pomodoro = () => {
   const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <>
-      {isPlaying && <Rain />}
+      <AnimatePresence>
+        {isPlaying && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: isPlaying ? 1 : 0 }}
+            exit={{ opacity: 0, transition: { duration: 1 } }} // Exit animation
+            transition={{ duration: 1 }} // Adjust duration as needed
+          >
+            <Rain />
+          </motion.div>
+        )}
+      </AnimatePresence>
       <Timer isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
     </>
   );

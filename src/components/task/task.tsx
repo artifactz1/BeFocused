43b64206b049fa3c0ab3ad6a76460db0b7571fd1
zachuri <punsalang.zachary@gmail.com>
2023-10-17 20:25:55 -0700

    task: updated ui with border and working task list

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -20,27 +20,81 @@ const Task = () => {
 
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <input
-        type="text"
-        placeholder="Add a task for this round..."
-        value={task}
-        onChange={e => setTask(e.target.value)}
-        className="border"
-      />
-      <div onClick={handleAddTask}>Add</div>
-      <ul>
-        {tasks.map(task => (
-          <>
-            <li key={task.id} className="flex flex-row justify-between gap-x-2">
-              <button onClick={() => toggleCompleted(task.id)}>
-                {task.completed ? 'completed' : 'not completed'}
-              </button>
-              <h1>{task.task}</h1>
-              <button onClick={() => removeTask(task.id)}>remove</button>
-            </li>
-          </>
-        ))}
-      </ul>
+      <div className="border border-black rounded-xl p-10">
+        <section className="flex flex-row space-x-5">
+          <input
+            type="text"
+            placeholder="Add a task ..."
+            value={task}
+            onChange={e => setTask(e.target.value)}
+            className="border"
+          />
+          <button className="flex flex-row" onClick={handleAddTask}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+          </button>
+        </section>
+
+        {tasks.length > 0 && (
+          <ul className="mt-10">
+            {tasks.map(task => (
+              <>
+                <li
+                  key={task.id}
+                  className="flex flex-row justify-between gap-x-2"
+                >
+                  <label className="flex items-center space-x-2">
+                    <input
+                      type="checkbox"
+                      className="text-indigo-600 h-5 w-5"
+                      checked={task.completed}
+                      onChange={() => toggleCompleted(task.id)}
+                    />
+
+                    <span
+                      className={
+                        task.completed
+                          ? 'line-through text-gray-500'
+                          : 'text-gray-700'
+                      }
+                    >
+                      {task.task}
+                    </span>
+                  </label>
+                  <button onClick={() => removeTask(task.id)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                      />
+                    </svg>
+                  </button>
+                </li>
+              </>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 };

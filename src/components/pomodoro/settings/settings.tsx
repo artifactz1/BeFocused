faeb9b8b10b6193d2ab1f52190ea95277a7f116b
zachuri <punsalang.zachary@gmail.com>
2023-10-19 18:56:09 -0700

    update-ui-shadcn: general ui dropdown menu setup

--- src/components/pomodoro/settings/settings.tsx
+++ src/components/pomodoro/settings/settings.tsx
@@ -1,5 +1,18 @@
-import React, { useEffect, useState } from "react";
-import ButtonInput from "./button";
+import React, { useEffect, useState } from 'react';
+import ButtonInput from './button';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Slider } from '@/components/ui/slider';
 
 interface SettingsProps {
   onSave: (values: any) => void;
@@ -14,13 +27,13 @@ const SettingsComponent: React.FC<SettingsProps> = ({ onSave }) => {
     shortBreak: 5,
     longBreak: 15,
     rounds: 4,
-    change: false,
+    change: false
   });
 
   const handleRangeValueChange = (newRangeValue: number, type: string) => {
-    setValues((prevValues) => ({
+    setValues(prevValues => ({
       ...prevValues,
-      [type]: newRangeValue,
+      [type]: newRangeValue
     }));
   };
 
@@ -30,73 +43,127 @@ const SettingsComponent: React.FC<SettingsProps> = ({ onSave }) => {
   };
 
   return (
-    <div className="bg-gray-400 w-full h-fit border-1 rounded-xl">
-      <div className="p-5 flex flex-col space-y-2">
-        <div className="border border-1 rounded-md w-fit p-2">Settings</div>
+    <DropdownMenu>
+      <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+      <DropdownMenuContent className="w-[400px]">
+        <DropdownMenuLabel>Settings</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Card className="w-full">
+            <CardHeader>Focus</CardHeader>
+            <CardContent>
+              <Slider
+                defaultValue={[values.focus]}
+                max={maxRangeMinutes}
+                onChange={e => handleRangeValueChange(e.target.value, 'focus')}
+                step={1}
+              />
+            </CardContent>
+          </Card>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Card className="w-full">
+            <CardHeader>Short Break</CardHeader>
+            <CardContent>
+              <Slider
+                defaultValue={[values.shortBreak]}
+                max={maxRangeMinutes}
+                onChange={e => handleRangeValueChange(e, 'focus')}
+                step={1}
+              />
+            </CardContent>
+          </Card>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Card className="w-full">
+            <CardHeader>Long Break</CardHeader>
+            <CardContent>
+              <Slider
+                defaultValue={[values.longBreak]}
+                max={maxRangeMinutes}
+                step={1}
+              />
+            </CardContent>
+          </Card>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Card className="w-full">
+            <CardHeader>Rounds</CardHeader>
+            <CardContent>
+              <Slider defaultValue={[values.rounds]} max={maxRounds} step={1} />
+            </CardContent>
+          </Card>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
 
-        <div className="flex border border-1 rounded-md p-4">
-          <div className="flex-initial w-1/3">Focus</div>
-          <div className="flex-1">
-            {/* Use maxRangeMinutes as the prop name */}
-            <ButtonInput
-              initialMaxRange={maxRangeMinutes}
-              initialRangeValue={values.focus}
-              onRangeValueChange={(value) =>
-                handleRangeValueChange(value, "focus")
-              }
-            />
-          </div>
-        </div>
+    // <div className="bg-gray-400 w-full h-fit border-1 rounded-xl">
+    //   <div className="p-5 flex flex-col space-y-2">
+    //     <div className="border border-1 rounded-md w-fit p-2">Settings</div>
 
-        <div className="flex border border-1 rounded-md p-4">
-          <div className="flex-initial w-1/3">Short Break</div>
-          <div className="flex-1">
-            <ButtonInput
-              initialMaxRange={maxRangeMinutes}
-              initialRangeValue={values.shortBreak}
-              onRangeValueChange={(value) =>
-                handleRangeValueChange(value, "shortBreak")
-              }
-            />
-          </div>
-        </div>
+    //     <div className="flex border border-1 rounded-md p-4">
+    //       <div className="flex-initial w-1/3">Focus</div>
+    //       <div className="flex-1">
+    //         {/* Use maxRangeMinutes as the prop name */}
+    //         <ButtonInput
+    //           initialMaxRange={maxRangeMinutes}
+    //           initialRangeValue={values.focus}
+    //           onRangeValueChange={(value) =>
+    //             handleRangeValueChange(value, "focus")
+    //           }
+    //         />
+    //       </div>
+    //     </div>
 
-        <div className="flex border border-1 rounded-md p-4">
-          <div className="flex-initial w-1/3">Long Break</div>
-          <div className="flex-1">
-            <ButtonInput
-              initialMaxRange={maxRangeMinutes}
-              initialRangeValue={values.longBreak}
-              onRangeValueChange={(value) =>
-                handleRangeValueChange(value, "longBreak")
-              }
-            />
-          </div>
-        </div>
+    //     <div className="flex border border-1 rounded-md p-4">
+    //       <div className="flex-initial w-1/3">Short Break</div>
+    //       <div className="flex-1">
+    //         <ButtonInput
+    //           initialMaxRange={maxRangeMinutes}
+    //           initialRangeValue={values.shortBreak}
+    //           onRangeValueChange={(value) =>
+    //             handleRangeValueChange(value, "shortBreak")
+    //           }
+    //         />
+    //       </div>
+    //     </div>
 
-        <div className="flex   border border-1 rounded-md p-4">
-          <div className="flex-initial w-1/3">Rounds</div>
-          <div className="flex-1">
-            <ButtonInput
-              initialMaxRange={maxRounds}
-              initialRangeValue={values.rounds}
-              onRangeValueChange={(value) =>
-                handleRangeValueChange(value, "rounds")
-              }
-            />
-          </div>
-        </div>
+    //     <div className="flex border border-1 rounded-md p-4">
+    //       <div className="flex-initial w-1/3">Long Break</div>
+    //       <div className="flex-1">
+    //         <ButtonInput
+    //           initialMaxRange={maxRangeMinutes}
+    //           initialRangeValue={values.longBreak}
+    //           onRangeValueChange={(value) =>
+    //             handleRangeValueChange(value, "longBreak")
+    //           }
+    //         />
+    //       </div>
+    //     </div>
 
-        <div className="flex flex-row-reverse">
-          <button
-            onClick={handleSave}
-            className="flex flex-row-reverse border border-1 rounded-md w-fit p-2 cursor-pointer"
-          >
-            Save
-          </button>
-        </div>
-      </div>
-    </div>
+    //     <div className="flex   border border-1 rounded-md p-4">
+    //       <div className="flex-initial w-1/3">Rounds</div>
+    //       <div className="flex-1">
+    //         <ButtonInput
+    //           initialMaxRange={maxRounds}
+    //           initialRangeValue={values.rounds}
+    //           onRangeValueChange={(value) =>
+    //             handleRangeValueChange(value, "rounds")
+    //           }
+    //         />
+    //       </div>
+    //     </div>
+
+    //     <div className="flex flex-row-reverse">
+    //       <button
+    //         onClick={handleSave}
+    //         className="flex flex-row-reverse border border-1 rounded-md w-fit p-2 cursor-pointer"
+    //       >
+    //         Save
+    //       </button>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
--- src/components/pomodoro/timer.tsx
+++ src/components/pomodoro/timer.tsx
@@ -4,6 +4,16 @@ import React, { useState, useEffect } from 'react';
 import WaveComponent from './wave';
 import SettingsComponent from './settings/settings';
 import { Button } from '../ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '../ui/dropdown-menu';
+import { Slider } from '../ui/slider';
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
 
 interface Props {
   isPlaying: boolean;
@@ -307,14 +317,12 @@ const Timer: React.FC<Props> = ({
             >
               <SettingsComponent onSave={handleSave} />
             </div>
-
             <button
               onClick={() => setIsButtonToggled(!isButtonToggled)}
               className="transition-all duration-300 ease-in-out bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 h-fit rounded"
             >
               Toggle Button
             </button>
-
             <button
               onClick={() => nextRound()}
               className="transition-all duration-300 ease-in-out bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 h-fit rounded"

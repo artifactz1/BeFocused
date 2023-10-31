1a0a23c12110f42b728e72ead2e857d76eb15d99
zachuri <punsalang.zachary@gmail.com>
2023-10-30 19:43:18 -0700

    layout: removed class attribute prop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider
-          attribute="class"
+          // attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange

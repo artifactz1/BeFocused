73172e7c6e6ac6cccd3692aaea81803bd7c87685
artifactz1 <punsalang.art@gmail.com>
2023-11-03 06:41:20 -0700

    sound: changed pause and play button

--- src/components/pomodoro/settings/rainsound.tsx
+++ src/components/pomodoro/settings/rainsound.tsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect, useState } from "react";
+import { Button } from "../../ui/button";
 
 interface Props {
   isPlaying: boolean;
@@ -134,7 +135,40 @@ const AudioPlayer: React.FC<Props> = ({ isPlaying, setIsPlaying }) => {
 
   return (
     <div>
-      <button onClick={toggleAudio}>{isPlaying ? "Pause" : "Play"}</button>
+      <Button onClick={toggleAudio}>
+        {isPlaying ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-4 h-4"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 5.25v13.5m-7.5-13.5v13.5"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-4 h-4"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+            />
+          </svg>
+        )}
+      </Button>
+
       {/* <p>Current Time: {currentTime.toFixed(2)} seconds</p>
       <p>Current Time A1: {currentTimeA1.toFixed(2)} seconds</p>
       <p>Current Time A2: {currentTimeA2.toFixed(2)} seconds</p> */}
